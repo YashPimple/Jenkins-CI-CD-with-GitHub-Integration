dae912b4a5cc0bff604def2e732b3df572c2989a
Yash Pimple <97302447+YashPimple@users.noreply.github.com>
2023-03-11 17:44:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express'),
     // In order to use PUT HTTP verb to edit item
     methodOverride = require('method-override'),
     // Mitigate XSS using sanitizer
-    sanitizer = require('sanitizer'),
+     sanitizer = require('sanitizer'),
     app = express(),
     port = 8000
 
